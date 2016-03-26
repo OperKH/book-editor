159ecec9ff97519abd0008884b4d61a18b24618c
OperKH <oper.kh@gmail.com>
2016-03-26 15:53:59 +0200

    Step 5

--- src/app/components/tree/tree.component.js
+++ src/app/components/tree/tree.component.js
@@ -8,4 +8,15 @@ export const treeComponent = {
 
 function treeControllerFn() {
     this.pagesListArray = Object.keys(this.pagesList);
+    this.openedNodes = [];
+    this.isOpened = pageName => this.openedNodes.includes(pageName);
+    this.toggleOpened = pageName => {
+        const { openedNodes } = this;
+        const index = openedNodes.indexOf(pageName);
+        if (index === -1) {
+            openedNodes.push(pageName);
+        } else {
+            openedNodes.splice(index, 1);
+        }
+    };
 }
